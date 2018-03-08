904db92e849941a69796b01a299a8bf7c3e713b9
Owen De Waele <owendewa@student.arteveldehs.be>
2018-03-08 08:57:14 +0100

    javascript for hovre effect on links

--- js/main.js
+++ js/main.js
@@ -22,15 +22,18 @@ $('a[href*="#"]')
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
+        
         });
       }
     }
   });
+
+  function border(){
+    document.getElementById('foto').style.border = "5px solid orange";
+    document.getElementById('foto').style.transform = "rotate(8deg)";
+  }
+  function normalImg(){
+    document.getElementById('foto').style.border = "5px solid #fff";
+    document.getElementById('foto').style.transform = "rotate(0deg)";
+
+  }
